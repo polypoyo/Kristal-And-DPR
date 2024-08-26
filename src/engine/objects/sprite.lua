f4ac8a3a3ba804b130d06313a69069c29d1cb573
AcousticJamm <jasmicheals@gmail.com>
2024-08-26 10:15:29 -0400

    Missing texture

--- src/engine/objects/sprite.lua
+++ src/engine/objects/sprite.lua
@@ -173,22 +173,39 @@ end
 --- *(Called internally)* Sets the current sprite to a single texture. \
 --- **Note**: *Only for internal overrides. Use `Sprite:setSprite()` instead.*
 function Sprite:setTextureExact(texture)
+
     if type(texture) == "string" then
         self.texture = Assets.getTexture(texture)
     else
         self.texture = texture
     end
-    if (not self.texture) and (texture ~= nil) then
-        Kristal.Console:warn("Texture not found: " .. Utils.dump(texture))
-    end
+
     self.texture_path = Assets.getTextureID(texture)
-    if self.use_texture_size then
-        if self.texture then
-            self.width = self.texture:getWidth()
-            self.height = self.texture:getHeight()
-        else
+    if not self.texture then
+        if texture ~= nil then
+            Kristal.Console:warn("Texture not found: " .. Utils.dump(texture))
+
+			if not (self.disallow_replacement_texture or (self.actor and self.actor.disallow_replacement_texture)) then
+				self.texture = Assets.getTexture("ui/missing_texture")
+				self.texture_path = self.texture_path or Assets.getTextureID(self.texture)
+				if self.width > 0 and self.height > 0 then
+					local resized_texture_canvas = Draw.pushCanvas(self.width, self.height)
+					Draw.draw(self.texture, 0, 0, 0, self.width/self.texture:getWidth(), self.height/self.texture:getHeight())
+					Draw.popCanvas()
+					self.texture = love.graphics.newImage(resized_texture_canvas:newImageData())
+				else
+					self.width = self.texture:getWidth()
+					self.height = self.texture:getHeight()
+				end
+			end
+        elseif self.use_texture_size then
             self.width = 0
             self.height = 0
+        end
+    else
+        if self.use_texture_size then
+            self.width = self.texture:getWidth()
+            self.height = self.texture:getHeight()
         end
     end
 end
