3e51cd62ce6c7652c42c9b1d5be4a9c433a08b98
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2024-10-07 22:16:54 -0700

    i may be stupid
    removed the dlc cause i wasn't supposed to throw it into the engine

--- mods/dpr_casino/scripts/battle/waves/empty.lua
+++
@@ -1,9 +0,0 @@
-local Empty, super = Class(Wave)
-
-function Empty:init()
-	super.init(self)
-	
-	self.time = 0
-end
-
-return Empty
--- mods/dpr_casino/scripts/world/maps/grey_cliffside/cliffside_start/map.lua
+++
@@ -1,9 +0,0 @@
-local map, super = Class(Map)
-
-function map:onEnter()
-    super.onEnter(self)
-    Game.world:spawnObject(white_glows(), "objects_bg")
-
-end
-
-return map
