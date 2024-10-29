07f4045228dd5496bad0d010abb7d8514159cabe
hyperboid <hyperboid@nullanoid.net>
2024-10-29 16:45:21 -0400

    Fix Mainline title

--- data/quests/mainline.lua
+++ data/quests/mainline.lua
@@ -2,7 +2,7 @@
 local MyQuest = Class(Quest, "mainline")
 
 function MyQuest:init()
-    self.name = Mod.info.name
+    self.name = "Dark Place: REBIRTH"
     self.description = "This is the Mainline quest. This is hardcoded into the library for the main story of your mod. The ID for this quest is 'mainline', so you can change the description by overriding it."
     self.progress = 0
     self.progress_max = 0
