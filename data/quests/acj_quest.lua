30c9ceed602b4da233d1ed00af7889d18c09c710
AcousticJamm <jasmicheals@gmail.com>
2024-10-29 21:40:00 -0400

    Added some quests of my own

---
+++ data/quests/acj_quest.lua
@@ -0,0 +1,21 @@
+---@class AcJQuest : Quest
+local AcJQuest, super = Class(Quest, "acj_quest")
+
+function AcJQuest:init()
+    super.init(self)
+    self.name = "AcousticFamily"
+    self.description = "AcousticJamm's daughter, Marcy, states that her father mentioned a \"TOMBSITE\" when leaving the room. Where could he have gone, and what does the TOMBSITE mean?"
+    self.progress = 0
+    self.progress_max = 0
+end
+
+function AcJQuest:getDescription()
+	if Game:getFlag("acj_quest_prog", 0) == 1 then
+		return "You found AcousticJamm in the forest. However, something seems off about him. Not only that, but the forest seems different than when you entered. Try to find your way out!"
+	elseif Game:getFlag("acj_quest_prog", 0) == 2 then
+		return "You found AcousticJamm (for real this time) during your battle against Enzio. Now, AcousticJamm is on your side! However, with Enzio, this may only be the start..."
+	end
+	return self.description
+end
+
+return AcJQuest
---
+++ data/quests/marcyquest.lua
@@ -0,0 +1,23 @@
+---@class MyQuest : Quest
+local MarcyQuest, super = Class(Quest, "marcyquest")
+
+function MarcyQuest:init()
+    super.init(self)
+    self.name = "Patch the Pirate"
+    self.description = "Marcy arrived on the ship her father went to, but not without punishment. Jamm lectured Marcy on the importance of not leaving the dark, and Marcy feels guilty. She thinks it would be best to talk to the captain."
+    self.progress = 0
+    self.progress_max = 0
+end
+
+function MarcyQuest:getDescription()
+	if Game:getFlag("marcyquest_prog", 0) == 1 then
+		return "Marcy talked to the captain and, as it turns out, she was able to join the crew for now. She decided it would be best if she took her time to explore for now."
+	elseif Game:getFlag("marcyquest_prog", 0) == 2 then
+		return "As the ship stops for maintenance, Jamm and Marcy decide to explore the stone island before them. This seems like the perfect opportunity for them to bond."
+	elseif Game:getFlag("marcyquest_prog", 0) == 3 then
+		return "After the ambush, Jamm brought Marcy to the ship for medical aid. As soon as Marcy wakes up, she learns that her father is currently fighting. She decides to help her father out."
+	end
+	return self.description
+end
+
+return MarcyQuest
