696e1613dbbb4be668f58fa1140da3c4a074111d
Simbel0 <simbel.user@gmail.com>
2024-11-10 23:58:04 +0100

    Little tweak

--- src/engine/tweaks.lua
+++ src/engine/tweaks.lua
@@ -22,3 +22,23 @@ string_metatable.__index = function (a, b)
         return string[b]
     end
 end
+
+-- "string is " .. true
+-- "string" .. {1, 2, 3}
+-- "string" .. Sprite()
+string_metatable.__concat = function (a, b)
+    -- Do not allow nil to be concatenated
+    if type(b) == "nil" then
+        return a..""
+    end
+
+    -- If it's a class, concatenate its name
+    if isClass(b) then
+        return a..Utils.getClassName(b)
+    -- If it's a table, dump it
+    elseif type(b) == "table" then
+        return a..Utils.dump(b)
+    end
+    -- Just convert it to a string
+    return a..tostring(b)
+end
