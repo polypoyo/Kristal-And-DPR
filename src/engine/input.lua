558406e39e0c0a1408ce08805479066d3e35f98a
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-11-09 23:20:20 -0500

    mobile: restructuring and custom sprites

--- src/engine/input.lua
+++ src/engine/input.lua
@@ -505,6 +505,9 @@ function Input.clear(key, clear_down)
     if key then
         local bindings = Input.getBoundKeys(key)
         if bindings then
+            self.key_pressed["mobile:"..key] = false
+            self.key_repeated["mobile:"..key] = false
+            self.key_released["mobile:"..key] = false
             for _,k in ipairs(bindings) do
                 local keys = type(k) == "table" and k or {k}
                 for _,l in ipairs(keys) do
@@ -702,6 +705,7 @@ end
 ---@return boolean
 function Input.down(key)
     local bindings = Input.getBoundKeys(key)
+    if Input.keyDown("mobile:"..key) then return true end
     if bindings then
         for _,k in ipairs(bindings) do
             if type(k) == "string" and Input.keyDown(k) then
@@ -742,6 +746,7 @@ end
 ---@param repeatable? boolean
 ---@return boolean
 function Input.pressed(key, repeatable)
+    if Input.keyPressed("mobile:"..key, repeatable) then return true end
     local bindings = Input.getBoundKeys(key)
     if bindings then
         for _,k in ipairs(bindings) do
@@ -795,6 +800,7 @@ end
 ---@param key string
 ---@return boolean
 function Input.released(key)
+    if Input.keyReleased("mobile:"..key) then return true end
     local bindings = Input.getBoundKeys(key)
     if bindings then
         for _,k in ipairs(bindings) do
@@ -880,6 +886,7 @@ end
 ---@param key string
 ---@return boolean
 function Input.is(alias, key)
+    if (alias == "mobile:"..key) then return true end
     if self.group_for_key[key] then
         key = self.group_for_key[key]
     end
