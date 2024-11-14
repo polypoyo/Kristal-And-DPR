a66d6d3404311a920080967f123613b8c51b11fe
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-11-14 18:26:18 -0500

    Discoball as part of DojoBG

--- mods/dpr_main/scripts/objects/battle/DojoBG.lua
+++ mods/dpr_main/scripts/objects/battle/DojoBG.lua
@@ -2,28 +2,27 @@ local BG, super = Class(Object)
 
 function BG:init(color, fill)
     super.init(self)
-    self.color = color
-    self.fill = fill or {0, 0, 0}
+    self.color = color or COLORS.white
+    self.fill = fill or COLORS.black
 	self.offset = 0
     self.speed = 0.5
     self.size = 50
     self.layer = BATTLE_LAYERS["bottom"]
-    Game.battle.discoball = Discoball()
-    Game.battle:addChild(Game.battle.discoball)
+    self.discoball = Discoball()
+    self:addChild(self.discoball)
 end
 
 function BG:update()
     super.update(self)
     self.fade = Game.battle.transition_timer / 10
     self.offset = self.offset + self.speed*DTMULT
-	
+	self.discoball.y = Utils.approach(self.discoball.y, 0, DT * 400)
     if self.offset >= 100 then
         self.offset = self.offset - 100
     end
 end
 
 function BG:draw()
-    super.draw(self)
 
     local r,g,b,a = unpack(self.fill)
     love.graphics.setColor(r,g,b, a or self.fade)
@@ -37,6 +36,7 @@ function BG:draw()
             love.graphics.draw(dojo, SCREEN_WIDTH/2, (SCREEN_HEIGHT/2)-75, 0, (2 + math.sin(self.offset/2) * 0.008), (2 + math.cos(self.offset/2) * 0.008), dojo:getWidth()/2, dojo:getHeight()/2)
         end
     end
+    super.draw(self)
 end
 
 return BG
