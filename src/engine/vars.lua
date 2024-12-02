67df753f7379e75932a75dd57a92b3a4b3c6c984
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-12-02 07:20:58 -0500

    add dark place event vars

--- src/engine/vars.lua
+++ src/engine/vars.lua
@@ -281,4 +281,9 @@ KRISTAL_EVENT = {
     onRegisterShops = "onRegisterShops", -- shop scripts finished registering / in: Registry.initShops() / passes: NONE / returns: NONE
     onRegisterMinigames = "onRegisterMinigames", -- minigame scripts finished registering / in: Registry.initMinigames() / passes: NONE / returns: NONE
     onRegistered = "onRegistered", -- all scripts finished registering / in: Registry.initialize(preload) / passes: NONE / returns: NONE
+
+    --dark place events--
+    onDPDessTalk = "onDPDessTalk", -- talked to dess in Dess - THE BEGINNING / in: dpr_main/scripts/world/cutscenes/desslmao.lua / passes: NONE / returns: NONE
+    onDPSusieCutscene = "onDPSusieCutscene",
+    onDPWarpBinUsed = "onDPWarpBinUsed",
 }
