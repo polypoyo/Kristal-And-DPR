c941d00ccf767d1e8e5ed6051114e14e21e19b90
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2024-12-08 18:22:13 -0500

    Fixed Dess plushie cutscene.

--- mods/dpr_main/scripts/world/cutscenes/plushies.lua
+++ mods/dpr_main/scripts/world/cutscenes/plushies.lua
@@ -1,16 +1,16 @@
 return {
     dess_plush = function(cutscene)
         local dess_plush = cutscene:getCharacter("dess_plush")
-        local dess = Game:hasPartyMember("dess")
-        local susie = Game:hasPartyMember("susie")
+        local dess = cutscene:getCharacter("dess")
+        local susie = cutscene:getCharacter("susie")
         if dess then
             cutscene:setSpeaker()
-            cutscene:textTagged("* Dang,[wait:5] I wonder how this got here.", "genuine", "dess")
-            cutscene:textTagged("* Dang,[wait:5] I REALLY wonder how this thing got here.", "kind", "dess")
-            cutscene:textTagged("* Good Heavens,[wait:5] I REALLY feel like pondering on how this marketable stuffed version of myself had made it's way to the location we currently are standing on.", "condescending", "dess")
+            cutscene:textTagged("* Dang,[wait:5] I wonder how this got here.", "genuine", dess)
+            cutscene:textTagged("* Dang,[wait:5] I REALLY wonder how this thing got here.", "kind", dess)
+            cutscene:textTagged("* Good Heavens,[wait:5] I REALLY feel like pondering on how this marketable stuffed version of myself had made it's way to the location we currently are standing on.", "condescending", dess)
             if susie then
-                cutscene:textTagged("* SHUT UP!!!", "teeth", "susie")
-                cutscene:textTagged("* k.", "condescending", "dess")
+                cutscene:textTagged("* OKAY, can we get moving now?!", "angry", susie)
+                cutscene:textTagged("* k.", "condescending", dess)
             end
         end
         cutscene:hideNametag()
